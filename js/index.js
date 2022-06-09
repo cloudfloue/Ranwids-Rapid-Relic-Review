83168fcb1aa7808e1613c4b9e7901add08c51fc2
lain h <87361804+kozashi6@users.noreply.github.com>
2022-06-09 16:33:17 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// why
+
+function startTimer() {
+
+}
+
+// cycle through the main menu, quiz proper and results screen questionTypes
+function switchQuestionType() {
+    event.preventDefault();
+
+    return false;
+}
+
+function generateMCQOptions() {
+    event.preventDefault();
+
+    return false;
+}
+
+function validateAnswer() {
+    event.preventDefault();
+
+    // temporary code
+    window.location.href = "./results.html";
+
+    return false;
+}
+
+function validateFormat() {
+    event.preventDefault();
+
+    return false;
+}
+
+function applyActiveState() {
+    event.preventDefault();
+
+    return false;
+}
